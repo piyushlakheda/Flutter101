82e7da8da12e3db8aa2d31c2a94d7b1258f96308
Your Name <piyushlakheda@gmail.com>
2022-05-15 20:59:04 +0530

    http call

--- flutter_application_1/lib/screens/homescreen.dart
+++ flutter_application_1/lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/secondScreen.dart';
@@ -14,7 +15,13 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(),
         body: Center(
           child: ElevatedButton(
-            onPressed: () {
+            onPressed: () async {
+              var url = Uri.parse(
+                  'https:goquotes-api-herokuaoo.com/api/v1/random?count=1');
+              var response = await http.post(url);
+              print('Response status: ${response.statusCode}');
+              print('Response body: ${response.body}');
+
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => secondScreen()));
             },
